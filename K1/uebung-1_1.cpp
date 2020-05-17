7c1516b9def2231b031cb9770080e53f1d40a9c0
Neil Soutar <soutar.n@gmail.com>
2020-05-17 14:15:40 +0200

    Übung 1.1 compiled

---
+++ K1/uebung-1_1.cpp
@@ -0,0 +1,17 @@
+//CPPBuch/K1/Übung 1.1
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  float x;
+  cout << "x eingeben:";
+  cin >> x;
+  cout << " x       = " << x       << '\n';
+  cout << " fabs(x) = " << fabs(x) << '\n';
+  cout << " sqrt(x) = " << sqrt(x) << '\n';
+  cout << " sin(x)  = " << sin(x)  << '\n';
+  cout << " exp(x)  = " << exp(x)  << '\n';
+  cout << " log(x)  = " << log(x)  << '\n';
+  return 0;
+}
